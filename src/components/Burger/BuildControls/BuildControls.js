da2af9a1142171f3f1d8efa50c45b94167abe5fe
samarjit mahi <41121431+Samarjiit@users.noreply.github.com>
2020-06-07 23:31:07 +0530

    adding functionality to burger

---
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -0,0 +1,27 @@
+import React from "react";
+import classes from "./BuildControls.module.css";
+import BuildControl from './BuildControl/BuildControl';
+import BurgerBuilder from "../../../containers/BurgerBuilder/BurgerBuilder";
+
+const controls = [
+  { label: "Salad", type: "salad" },
+  { label: "Bacon", type: "bacon" },
+  { label: "Meat", type: "meat" },
+  { label: "Cheese", type: "cheese" },
+];
+const BuildControls = (props) => (
+	<div className={classes.BuildControls}>
+		<p>current price:<strong>{props.price.toFixed(2)}</strong></p>
+
+		{controls.map(ctrl=>(
+			<BuildControl key={ctrl.label} 
+			label={ctrl.label}
+			added={()=>props.ingredientAdded(ctrl.type)}
+			removed={()=>props.ingredientRemoved(ctrl.type)}
+			disabled={props.disabled[ctrl.type]}
+			/>
+		))}
+	</div>
+);
+
+export default BuildControls;
--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,16 +4,24 @@ import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
 import { object } from "prop-types";
 
 const burger = (props) => {
-  const transformedIngredients = Object.keys(props.ingredients).map((igKey) => {
+  let transformedIngredients = Object.keys(props.ingredients).map((igKey) => {
+    //transfrom object into array
     return [...Array(props.ingredients[igKey])].map((_, i) => {
-      return  <BurgerIngredient key={igKey + i} type={igKey} />;
+      return <BurgerIngredient key={igKey + i} type={igKey} />;
     });
-  });
+  }).reduce((arr,el)=>{             //arr-previous value and el-current value.it's these inner arrays which are interesting to us. What we can do is we can simply flatten this array to make sure we pull out the values of these inner arrays and trade one array only which contains all these values, we can do that by adding reduce to our transformedIngredients logic here, reduce is a built-in array function which allows us to transform an array into something else. It takes a function as an input and this function receives two arguments passed in automatically by javascript, the previous value and the current value.it also accepts an initial value,let's say an empty array.So the initial value of the reduced value
+    return arr.concat(el)
+  },[]);
+  if(transformedIngredients.length===0){
+    transformedIngredients =<p>please starting adding ingredients!</p>;
+  }
+
+  console.log(transformedIngredients);
   return (
     <div className={classes.Burger}>
       <BurgerIngredient type="bread-top" />
 
-    {transformedIngredients}
+      {transformedIngredients}
 
       <BurgerIngredient type="bread-bottom" />
     </div>
